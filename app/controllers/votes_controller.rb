338a2295cae410008f71dfb1d631ac52fbc946a1
SamGoldman91 <samgoldman@chicagobooth.edu>
2021-12-07 19:57:57 +0000

    My Activity page up and running

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -11,6 +11,13 @@ class VotesController < ApplicationController
   
   #AND THIS
   def show_activity
+
+    #Pulling tips related to a particular user
+    @tips_for_user = Tip.where({ :user_id => @current_user }).order({ :created_at => :desc })
+
+    #Pulling votes related to a particular user
+    @votes_for_user = Vote.where({ :user_id => @current_user }).order({ :created_at => :desc })
+
     render({ :template => "activities/show.html.erb" })
   end
 
